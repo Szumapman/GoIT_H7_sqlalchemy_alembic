b27b6d652da54e645c3d812acb91526c4b3f6a76
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-18 04:27:30 +0100

    Update db_session.py

--- db_session.py
+++ db_session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:myPass@localhost:5432/university")
+engine = create_engine("postgresql://myUser:myPass@localhost:5432/university")
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
